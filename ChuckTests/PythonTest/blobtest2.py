56e8ecd82ee9ec8c5fa0fa21e08c893603eba024
Charles Burnell <charlesburnell@u.boisestate.edu>
2016-06-23 14:54:46 -0600

    major axis and added tests

--- ChuckTests/PythonTest/blobtest2.py
+++ ChuckTests/PythonTest/blobtest2.py
@@ -3,13 +3,15 @@
 # maybe spin this off as its own function so it can be used
 # in other functions
 # file
-filename = 'testnoise3.jpg'
+filename = 'testnoise2.jpg'
 # Standard imports
 import cv2
 import numpy as np
+
 from math import cos
 from math import sin
 from math import radians
+from math import sqrt
 # this can be improved
 # doing the vector form of lines but i dont want to rederive that
 # so fix this sometime in the future
@@ -19,7 +21,7 @@ def drawLinesBetweenPoints(image, pointlist, colorArray):
         point1 = (int(pointlist[i][0]),int(pointlist[i][1]))
         point2 = (int(pointlist[(i+1)%len(pointlist)][0]),int(pointlist[(i+1)%len(pointlist)][1]))
         #print (point1, point2)
-        cv2.line(image, point1, point2, colorArray, 2)
+        cv2.line(image, point1, point2, colorArray, 1)
         '''
         pointx1 = pointlist[i][0]
         pointx2 = pointlist[(i+i)%len(pointlist)][0]
@@ -31,30 +33,53 @@ def drawLinesBetweenPoints(image, pointlist, colorArray):
         currenty = pointy1
         '''
 def radialSearch(x, y, edgeMat):
-    edgemaxx = len(edgeMat)
-    edgemaxy = len(edgeMat[0])
+    edgemaxx = len(edgeMat[0])
+    edgemaxy = len(edgeMat)
     if edgeMat[y, x] != 0:
         return
     pointEdgeArray = []
-    numberOfSections = 18
+    numberOfSections = 36
     degreeStep = 360 / numberOfSections
-    edgeBuf = 15
+    edgeBuf = 10
     for i in xrange(0,360 , degreeStep):
         sinStep = sin(radians(i))
         cosStep = cos(radians(i))
         #print (cosStep, sinStep, i)
         currentx = x
         currenty = y
-        while (edgeMat[int(round(currenty)),int(round(currentx))] == 0):
+        #print "before"
+        #print edgeMat[int(round(currenty)),int(round(currentx))]
+        while (int(edgeMat[int(round(currenty)),int(round(currentx))]) == 0):
+            #print (edgeMat[int(round(currenty)),int(round(currentx))], currentx, currenty)
             currentx += cosStep
             currenty += sinStep
-            if currentx>edgemaxx or currenty> edgemaxy or currentx < 0 or currenty < 0:
+            if round(currentx)>=edgemaxx or round(currenty)>= edgemaxy or round(currentx) < 0 or round(currenty) < 0:
                 break
-            pointEdgeArray.append((currentx ,currenty))
-            print (currentx,currenty)
+            #pointEdgeArray.append((currentx ,currenty))
+            #print (currentx,currenty)
         pointEdgeArray.append((currentx + edgeBuf*cosStep ,currenty +edgeBuf*sinStep))
-        edgeMat[int(currenty),int(currentx)] = int(255/2)
+        #edgeMat[int(currenty),int(currentx)] = int(255/2)
     return pointEdgeArray, edgeMat
+
+def findMaxMinAxis(pointlist, image):
+    maxDist = 0
+    maxPoints = []
+    minDist = sqrt(len(image)*len(image) + len(image[0])*len(image[0]))
+    print len(image)
+    print len(image[0])
+    print minDist
+    minPoints =[]
+    for point1 in pointlist:
+        for point2 in pointlist:
+            distanceBetween = sqrt(pow(point1[0]-point2[0],2)+pow(point1[1]-point2[1],2))
+            if distanceBetween>maxDist:
+                maxDist = distanceBetween
+                maxPoints = (point1,point2)
+            elif distanceBetween<minDist:
+                minPoints = (point1,point2)
+                minDist = distanceBetween
+    drawLinesBetweenPoints(image,maxPoints,[0,100,100])
+    drawLinesBetweenPoints(image,minPoints,[0,200,200])
 
 # Read image
 im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -102,6 +127,7 @@ for point in keypointsBlur:
     pointx = pointxy[0]
     pointy = pointxy[1]
     im_with_keypointsBlur[pointy,pointx]=[0, 255, 0]
+    cannyEdges = cv2.GaussianBlur(cannyEdges,(3,3),0)
     pointEdgeArray, cannyEdges = radialSearch(pointx, pointy, cannyEdges)
     #print pointEdgeArray
     for edgePoint in pointEdgeArray:
@@ -110,8 +136,12 @@ for point in keypointsBlur:
         edgePointy = edgePoint[1]
         for i in xrange(- plusminus, plusminus):
             for j in xrange(-plusminus,plusminus):
-                im_with_keypointsBlur[edgePointy+j,edgePointx+i] = [255,0,0]
-    #drawLinesBetweenPoints(im_with_keypointsBlur, pointEdgeArray, [255,0,0])
+                try:
+                    im_with_keypointsBlur[edgePointy+j,edgePointx+i] = [120,120,0]
+                except:
+                    pass
+    findMaxMinAxis(pointEdgeArray,im_with_keypointsBlur)
+    drawLinesBetweenPoints(im_with_keypointsBlur, pointEdgeArray, [255,0,0])
 #    print (im_with_keypointsBlur[pointy,pointx])
 #    print (point.pt)
 
